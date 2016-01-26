70af9836c4d5ccd5313fec9b50176ea7516d286f
Robert Milasan <rmilasan@suse.com>
2016-01-26 09:34:27 +0100

    fix short url

--- qr-redirect.php
+++ qr-redirect.php
@@ -6,7 +6,7 @@ if (!empty($_GET['alias'])) {
     if (preg_match("/^[a-zA-Z0-9]+$/", $code) && code_exists($code) !== false) {// Allows creation/show of QR only if it's sane
         if (!file_exists('qrcache/'.$code.'.png')) {                            // Creates QR only if it doesn't already exist in cache
             require_once('include/qr/qrlib.php');
-            QRcode::png(SITE_URL.$code, 'qrcache/'.$code.'.png', 'L', 8, 2);    // Write QR to cache file
+            QRcode::png(SITE_URL.'/'.$code, 'qrcache/'.$code.'.png', 'L', 8, 2);    // Write QR to cache file
         }
 	echo '<img src="qrcache/'.$code.'.png">';
         exit;
--- qr.php
+++ qr.php
@@ -6,7 +6,7 @@ if (!empty($_GET['alias'])) {
     if (preg_match("/^[a-zA-Z0-9]+$/", $code)) {// Allows creation/show of QR only if it's sane
         if (!file_exists('qrcache/'.$code.'.png')) {                            // Creates QR only if it doesn't already exist in cache
             require_once('include/qr/qrlib.php');
-            QRcode::png(SITE_URL.$code, 'qrcache/'.$code.'.png', 'L', 8, 2);    // Write QR to cache file
+            QRcode::png(SITE_URL.'/'.$code, 'qrcache/'.$code.'.png', 'L', 8, 2);    // Write QR to cache file
         }
         header('Content-Type: image/png');                                      // Override mime type header
         echo file_get_contents('qrcache/'.$code.'.png');                        // Print out the QR from file
