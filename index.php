202adeb8893819967b0ecfd1a244728b6dc0a1d8
Robert Milasan <robert.milasan@zoomint.com>
2016-02-23 09:11:49 +0100

    add 'Fork Me on Github' in index.php

--- index.php
+++ index.php
@@ -16,8 +16,9 @@ function html_obfuscate($text) {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <script type="text/javascript" src="include/script.js"></script>
-</head><body><table>
-<tr><td colspan="3" id="header"><div align="center">
+</head><body>
+<a href="https://github.com/robertalks/buh.bz"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+<table><tr><td colspan="3" id="header"><div align="center">
 <a href="<?php echo SITE_URL; ?>"><img src="<?php echo SITE_IMAGE; ?>" alt="<?php echo SITE_NAME; ?>" /></a><h1><a href="<?php echo SITE_URL; ?>">Buuuh!</a></h1><br/>
 </div>
 </td></tr>
